8186f37e9bf775ab0a77b0e6b78fd236d940a9a9
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-04-11 19:09:08 +0200

    Creando proyecto

---
+++ src/main/java/es/mde/mipapeleria/MipapeleriaApplication.java
@@ -0,0 +1,13 @@
+package es.mde.mipapeleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MipapeleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MipapeleriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/mde/mipapeleria/MipapeleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.mipapeleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MipapeleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
