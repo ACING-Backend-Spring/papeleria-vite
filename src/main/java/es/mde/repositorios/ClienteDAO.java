97b72383924a809ccde60b3405e8d5c3e729df7f
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-04-11 20:09:43 +0200

    Personalizando endpoints con @RestResource

--- src/main/java/es/mde/repositorios/ClienteDAO.java
+++ src/main/java/es/mde/repositorios/ClienteDAO.java
@@ -1,11 +1,24 @@
 package es.mde.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import es.mde.entidades.Cliente;
 
 @RepositoryRestResource(path = "clientes", itemResourceRel = "cliente", collectionResourceRel = "clientes")
 public interface ClienteDAO extends JpaRepository<Cliente, Long> {
+	@RestResource(path = "nombre")
+	List<Cliente> findByNombreIgnoreCaseContaining(String txt);
 
+	@RestResource(path = "correo")
+	List<Cliente> findByCorreoContaining(String txt);
+
+	@RestResource(exported = false)
+	void deleteById(Long id);
+
+	@RestResource(exported = false)
+	void delete(Cliente cliente);
 }
