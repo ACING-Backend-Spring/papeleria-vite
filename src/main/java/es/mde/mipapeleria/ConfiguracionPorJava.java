e6c33f112fc6719f7d7ed82323cc9d04db759555
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-04-12 02:37:12 +0200

    Configuracion Rest

--- src/main/java/es/mde/mipapeleria/ConfiguracionPorJava.java
+++ src/main/java/es/mde/mipapeleria/ConfiguracionPorJava.java
@@ -3,15 +3,7 @@ package es.mde.mipapeleria;
 import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.Properties;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
-import java.lang.reflect.Method;
-import java.net.URI;
-import java.net.URLDecoder;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -24,23 +16,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.RepositorySearchesResource;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.server.RepresentationModelProcessor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import es.mde.entidades.Cliente;
 import es.mde.entidades.Cuaderno;
 import es.mde.entidades.Libro;
-import es.mde.entidades.Producto;
-import es.mde.rest.ClienteController;
+
 import es.mde.rest.MixIns;
-import es.mde.rest.ProductoController;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 
@@ -98,58 +82,4 @@ public class ConfiguracionPorJava {
 		return mapper;
 	}
 
-	/**
-	 * Enlaza automaticamente los links de los controladores registrados siguiendo
-	 * las <a href=
-	 * "https://www.hijosdelspectrum.com/2020/05/codigo-util-clase-configuracionrest.html">instrucciones
-	 * </a>
-	 * 
-	 * @param config {@link RepositoryRestConfiguration} para recuperar al
-	 *               {@code basePath}
-	 * @return el bean del tipo
-	 *         {@code RepresentationModelProcessor<RepositorySearchesResource>}
-	 */
-	@Bean
-	RepresentationModelProcessor<RepositorySearchesResource> addSearchLinks(RepositoryRestConfiguration config) {
-		Map<Class<?>, Class<?>> controllersRegistrados = new HashMap<>();
-		controllersRegistrados.put(Producto.class, ProductoController.class);
-		controllersRegistrados.put(Cliente.class, ClienteController.class);
-
-		return new RepresentationModelProcessor<RepositorySearchesResource>() {
-
-			@Override
-			public RepositorySearchesResource process(RepositorySearchesResource searchResource) {
-				if (controllersRegistrados.containsKey(searchResource.getDomainType())) {
-					Class<?> controller = controllersRegistrados.get(searchResource.getDomainType());
-					Method[] metodos = controller.getDeclaredMethods();
-					URI uriController = linkTo(controller).toUri();
-					String controllerPath = config.getBasePath() + uriController.getPath();
-					Stream.of(metodos).filter(
-							m -> m.isAnnotationPresent(ResponseBody.class) && m.isAnnotationPresent(GetMapping.class))
-							.map(m -> {
-								Link link = null;
-								try {
-									String pathMetodo = String.join("", m.getAnnotation(GetMapping.class).value());
-									String pathRecurso = new URI(uriController.getScheme(), uriController.getUserInfo(),
-											uriController.getHost(), uriController.getPort(),
-											controllerPath + pathMetodo, null, null).toString();
-									String requestParams = Stream.of(m.getParameters())
-											.filter(p -> p.isAnnotationPresent(RequestParam.class)).map(p -> {
-												String nombreParametro = p.getAnnotation(RequestParam.class).value();
-												return !"".equals(nombreParametro) ? nombreParametro : p.getName();
-											}).collect(Collectors.joining(","));
-									link = Link.of(URLDecoder.decode(pathRecurso, "UTF-8") + "{?" + requestParams + "}",
-											m.getName());
-								} catch (Exception e) {
-									e.printStackTrace();
-								}
-								return link;
-							}).filter(l -> l != null).forEach(searchResource::add);
-				}
-
-				return searchResource;
-			}
-
-		};
-	}
 }
