735358a32e73ce9dca5e940fee71925d2f98e9c6
joseluispuentes82 <joseluispuentes@hotmail.com>
2025-04-12 02:41:39 +0200

    Preparando para conectar a BBDD (nube-SQL EXPRESS)

--- src/main/java/es/mde/mipapeleria/ConfiguracionPorJava.java
+++ src/main/java/es/mde/mipapeleria/ConfiguracionPorJava.java
@@ -30,8 +30,9 @@ import jakarta.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource({ "classpath:config/rest.properties", "classpath:config/jackson.properties" })
-@EnableJpaRepositories("${misRepositorios}") // leer valor de propiedades? pero solo para las entidades anotadas
+@PropertySource({ "classpath:config/rest.properties", "classpath:config/jackson.properties", "classpath:config/gestionBBDD.properties"
+	, "classpath:config/passwordsBD.properties" 
+	})@EnableJpaRepositories("${misRepositorios}") // leer valor de propiedades? pero solo para las entidades anotadas
 @ComponentScan({ "es.mde.repositorios", "es.mde.rest" }) // para que escanee los Listener y los Controller...
 public class ConfiguracionPorJava {
 
